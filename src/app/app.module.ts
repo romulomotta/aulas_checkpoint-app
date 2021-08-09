5f5186aa7d627e9f6332e012efd979c6a54d6786
Romulo Motta <pukpen@gmail.com>
2021-08-08 21:17:59 -0300

    teste clientes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,30 @@
+import { ClientesService } from './clientes.service';
+import { ClientesModule } from './clientes/clientes.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    TemplateModule,
+    ClientesModule
   ],
-  providers: [],
+  providers: [
+    ClientesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clientes.service.ts
@@ -0,0 +1,23 @@
+import { Cliente } from './clientes/cliente';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientesService {
+
+  constructor( private http: HttpClient) {}
+
+  salvar( cliente: Cliente ) : Observable<Cliente> {
+    return this.http.post<Cliente>('http://localhost:8080/api/clientes', cliente);
+  }
+
+  getCliente(): Cliente {
+    let cliente : Cliente = new Cliente();
+    cliente.nome = 'Fulano de Tal';
+    cliente.cpf = '88888888888';
+    return cliente;
+  }
+}
